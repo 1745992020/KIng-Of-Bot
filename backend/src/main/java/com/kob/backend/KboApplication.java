c0557285cf58bb74b29f4f3cc2be442b6690e26b
wys <302771233@qq.com>
2023-11-09 16:30:28 +0800

    创建项目

---
+++ backend/src/main/java/com/kob/backend/KboApplication.java
@@ -0,0 +1,13 @@
+package com.kob.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KboApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KboApplication.class, args);
+    }
+
+}
